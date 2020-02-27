a0cf01f570023de3ca2af1f3b9e175f47a9f7801
ambujalpha <jainambuj8@gmail.com>
2020-02-27 23:56:52 +0530

    updates

--- music/views.py
+++ music/views.py
@@ -1,9 +1,17 @@
-from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render
+from .models import Album
 
 
 def index(request):
-    return HttpResponse("<h1>This is a music app homepage</h1>")
+    all_albums = Album.objects.all()
+    context = {'all_albums': all_albums}
+    return render(request, 'music/index.html', context)
 
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for album_id: " + str(album_id) + "</h2>")
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album dose not exist")
+    return render(request, 'music/detail.html', {'album': album})
