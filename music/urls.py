ca0b4c279a0d0fa5ae6b6b2a2782bfbc013961ac
ambujalpha <jainambuj8@gmail.com>
2020-02-28 11:30:23 +0530

    detail_page_fav

--- music/urls.py
+++ music/urls.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+app_name = 'music'
+
 urlpatterns = [
+
+    # /music/
     path('', views.index, name='index'),
+
+    # /music/<album_id>/
     path('<int:album_id>/', views.detail, name='detail'),
+
+    # /music/<album_id>/favourite
+    path('<int:album_id>/favourite/', views.favourite, name='favourite'),
 ]
